9def193daedab0bb4357e12ffc7d873c9f2fc53f
Yan Hwee <yanhweeyee@gmail.com>
2025-10-12 01:28:05 +0800

    Revert "Add vercel speed insights"
    This reverts commit d153df3e295cc81fdbb6f43a5927d6428b49e6c1.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 import App from "@/components/App/App"
 import ClientRender from "@/components/ClientRender/ClientRender"
 import { getLibraryInfo } from "@/lib/server"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default async function Home() {
   const libraryInfo = await getLibraryInfo()
 
   return (
-    <>
-      <SpeedInsights />
-      <ClientRender>
-        <App libraryInfo={libraryInfo} />
-      </ClientRender>
-    </>
+    <ClientRender>
+      <App libraryInfo={libraryInfo} />
+    </ClientRender>
   )
 }
