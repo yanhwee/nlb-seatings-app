d153df3e295cc81fdbb6f43a5927d6428b49e6c1
Yan Hwee <yanhweeyee@gmail.com>
2025-10-12 01:02:24 +0800

    Add vercel speed insights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import App from "@/components/App/App"
 import ClientRender from "@/components/ClientRender/ClientRender"
 import { getLibraryInfo } from "@/lib/server"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default async function Home() {
   const libraryInfo = await getLibraryInfo()
 
   return (
-    <ClientRender>
-      <App libraryInfo={libraryInfo} />
-    </ClientRender>
+    <>
+      <SpeedInsights />
+      <ClientRender>
+        <App libraryInfo={libraryInfo} />
+      </ClientRender>
+    </>
   )
 }
