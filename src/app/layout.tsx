162638c07ff4e0c5fa076a72677fd19ee7546149
Yan Hwee <yanhweeyee@gmail.com>
2025-10-14 00:04:04 +0800

    Update vercel insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import "./globals.css"
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({
   children,
@@ -10,7 +12,11 @@ export default function RootLayout({
       <head>
         <title>NLB Seatings</title>
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
 import App from "@/components/App/App"
 import ClientRender from "@/components/ClientRender/ClientRender"
 import { getLibraryInfo } from "@/lib/server"
-import { Analytics } from "@vercel/analytics/next"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default async function Home() {
   const libraryInfo = await getLibraryInfo()
 
   return (
     <>
-      <Analytics />
-      <SpeedInsights />
       <ClientRender>
         <App libraryInfo={libraryInfo} />
       </ClientRender>
